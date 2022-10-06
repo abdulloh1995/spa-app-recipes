fd9f2f9dfc142a1d464fca9ac7814587e298b940
Abdulloh <developer.sobirov4448@gmail.com>
2022-10-06 14:13:39 +0300

    end

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,18 +2,38 @@ import React, {useEffect, useState} from 'react';
 import {getAllCategories} from "../api/api";
 import Loader from "../components/Loader";
 import CategoryList from "../components/CategoryList";
+import Search from "../components/Search";
+import {useHistory, useLocation} from "react-router-dom";
 
 const Home = () => {
     const [catalog, setCatalog] = useState([]);
+    const [filteredCatalog, setFilteredCatalog] = useState([]);
+
+    const {pathname, search} = useLocation()
+    const {push} = useHistory()
+
+    const handleSearch = (str) => {
+        setFilteredCatalog(catalog.filter(item => item.strCategory.toLowerCase().includes(str.toLowerCase())))
+        push({
+            pathname,
+            search: `?s=${str}`
+        })
+    }
 
     useEffect(() => {
         getAllCategories().then(data => {
             setCatalog(data.categories)
+            setFilteredCatalog(search ?
+                data.categories.filter(item =>
+                item.strCategory
+                    .toLowerCase()
+                    .includes(search.split("=")[1].toLowerCase())) : data.categories)
         })
-    }, []);
+    }, [search]);
     return (
         <div>
-            {!catalog.length ? (<Loader/>) : (<CategoryList catalog={catalog}/>)}
+            <Search cb={handleSearch} />
+            {!catalog.length ? (<Loader/>) : (<CategoryList catalog={filteredCatalog}/>)}
         </div>
     );
 };
--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -5,6 +5,7 @@ import Loader from "../components/Loader";
 
 const Recipe = () => {
     const [recipe, setRecipe] = useState([]);
+    const [showRecipe, setShowRecipe] = useState(false);
     const {id} = useParams()
     const {goBack} = useHistory()
 
@@ -13,39 +14,43 @@ const Recipe = () => {
     }, []);
 
     return (<div>
-            <button className="btn" onClick={goBack}>Go Back</button>
-            {!recipe.idMeal ? <Loader/> : (<div className="recipe">
-                    <img src={recipe.strMealThumb} alt={recipe.strMeal}/>
-                    <h1>{recipe.strMeal}</h1>
-                    <h6><b>Category:</b> {recipe.strCategory}</h6>
-                    {!recipe.strArea ? null : <h6><b>Area:</b> {recipe.strArea}</h6>}
-                    <p>{recipe.strInstructions}</p>
-                    <table className="highlight">
-                        <thead>
-                        <tr>
-                            <th>Ingredient</th>
-                            <th>Measure</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {Object.keys(recipe).map(key => {
-                            if (key.includes('Ingredient') && recipe[key]) {
-                                return (
-                                    <tr>
-                                        <td>{recipe[key]}</td>
-                                        <td>{recipe[`strMeasure${key.slice(13)}`]}</td>
-                                    </tr>
-                                )
-                            }
-                        })}
-                        </tbody>
-                    </table>
-                    {!recipe.strYoutube ? null : (<div className="row">
-                            <h5>Video Recipe</h5>
-                            <iframe src={`https://www.youtube.com/embed/${recipe.strYoutube.slice(-11)}`} title={id}/>
-                        </div>)}
-                </div>)}
-        </div>);
+        <button className="btn" onClick={goBack}>Go Back</button>
+        {!recipe.idMeal ? <Loader/> : (<div className="recipe">
+            <img src={recipe.strMealThumb} alt={recipe.strMeal}/>
+            <h1>{recipe.strMeal}</h1>
+            <h6><b>Category:</b> {recipe.strCategory}</h6>
+            {!recipe.strArea ? null : <h6><b>Area:</b> {recipe.strArea}</h6>}
+            <p>{recipe.strInstructions}</p>
+            <button className="btn" onClick={() => setShowRecipe(!showRecipe)}>Show Recipe</button>
+            {!showRecipe ? null : (
+                <table className="highlight">
+                    <thead>
+                    <tr>
+                        <th>Ingredient</th>
+                        <th>Measure</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {Object.keys(recipe).map(key => {
+                        if (key.includes('Ingredient') && recipe[key]) {
+                            return (
+                                <tr>
+                                    <td>{recipe[key]}</td>
+                                    <td>{recipe[`strMeasure${key.slice(13)}`]}</td>
+                                </tr>
+                            )
+                        }
+                    })}
+                    </tbody>
+                </table>
+            )}
+
+            {!recipe.strYoutube ? null : (<div className="row">
+                <h5>Video Recipe</h5>
+                <iframe src={`https://www.youtube.com/embed/${recipe.strYoutube.slice(-11)}`} title={id}/>
+            </div>)}
+        </div>)}
+    </div>);
 };
 
 export default Recipe;
